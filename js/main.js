50531172f30591934b31691f3484afa7c131dcaf
David Saymon <davidh_saymon@hotmail.com>
2024-03-19 18:45:19 -0300

    Primeiro commit

---
+++ js/main.js
@@ -0,0 +1,41 @@
+$(document).ready(function() {
+    $('#telefone').mask('(00) 00000-0000');
+    $('#cpf').mask('000.000.000-00', {reverse: true});
+    $('#cep').mask('00000-000');
+
+    $('form').validate( {
+        rules: {
+            nome: {
+                required: true
+            },
+            email: { 
+                required: true
+            },
+            telefone: {
+                required: true
+            },
+            cpf: {
+                required: true
+            },
+            endereco: {
+                required: true
+            },
+            cep: {
+                required: true
+            }
+        },
+        messages: {
+            nome: 'Por favor, preencha o campo'
+        },
+        submitHandler: function(form) {
+            console.log(form);
+            form.submit();
+        },
+        invalidHandler: function(evento, validador) {
+            let camposIncorretos = validador.numberOfInvalids();
+            if (camposIncorretos) {
+                alert(`Existem ${camposIncorretos} campos incorretos`);
+            }
+        }
+    });
+});
